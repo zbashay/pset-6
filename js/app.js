5027ae1956b7b2fd5354bd94350a582b97ef7654
zbashay <zbashay@ucvts.org>
2020-01-05 18:00:04 -0500

    ok

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 let elements = []
 var add = document.getElementById("add_button");
 var row = document.getElementsByClassName("row");
+let list_item = document.getElementsByClassName("to_do")
 let priority_button = document.getElementsByClassName("priority_button");
 let complete_button = document.getElementsByClassName("complete_button");
 let remove_button = document.getElementsByClassName("remove_button");
-let list_item = document.getElementsByClassName("to_do")
+
 
 let element_prioritize;
 
